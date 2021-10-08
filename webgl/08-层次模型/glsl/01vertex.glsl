ad3bcf7ef40812e0d1be3cb091e675aff58488a8
123wg <2367348689@qq.com>
2021-10-08 23:13:12 +0800

    feat:机械手臂增加光照

--- webgl/08-层次模型/glsl/01vertex.glsl
+++ webgl/08-层次模型/glsl/01vertex.glsl
@@ -1,8 +1,15 @@
 attribute vec4 a_Position; // 位置
 uniform mat4 u_MvpMatrix;// 模型视图投影矩阵
+uniform vec3 u_LightDirection;// 平行光方向
+uniform vec3 u_LightColor;// 光照颜色
+attribute vec4 a_Normal;// 法向量
+uniform mat4 u_NormalMatrix;// 逆转置矩阵
 attribute vec4 a_Color;
 varying vec4 v_Color;
 void main(){
     gl_Position = u_MvpMatrix * a_Position;
-    v_Color = a_Color;
+    vec3 normal  = normalize(vec3(u_NormalMatrix * a_Normal)); // 法向量归一化
+    float nDot = max(dot(u_LightDirection,normal),0.0);
+    vec3 diffuse = u_LightColor * vec3(a_Color) * nDot;
+    v_Color = vec4(diffuse,a_Color.a);
 }
